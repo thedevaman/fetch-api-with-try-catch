294c54ff1ff051c8a748d5e69f847cf0d0aadf32
unknown <devaman2508@gmail.com>
2024-09-09 22:07:28 +0530

    fetch product api and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 const App = ()=>{
 
  const [photos,setPhotos] = useState([])
+ useEffect(()=>test,[])
 
   // const test =() =>{
     // fetch('https://jsonplaceholder.typicode.com/photos')
@@ -17,11 +18,11 @@ const App = ()=>{
     // })
 // }
 const test = async ()=>{
-    try{
-     const response = await fetch('https://jsonplaceholder.typicode.com/photos')
-     const data =  response.json()
-     console.log(data)
-    //  setPhotos(data)
+    try {
+     const response = await fetch('https://fakestoreapi.com/products')
+     const data = await response.json()
+    //  console.log(data)
+      setPhotos(data)
     }
     catch(error){
       console.log(error)
@@ -33,8 +34,30 @@ const test = async ()=>{
     <div>
       <h1>Http Request</h1>
       <button onClick={test}>Click Me</button>
-     <div style={{marginTop:100,width:'80%'}}>
-   
+     <div style={
+      {marginTop:100,
+      width:'100%',
+      display:'flex',
+      gap:32,
+      flexWrap:'wrap'
+      }}>
+      {
+        photos.map((item,index)=>(
+          <div key={index} style={{width:"calc(25% - 32px)",
+            padding:16,
+            boxSizing:'border-box',
+            boxShadow:'0 0 8px #ddd',
+            borderRadius:8,
+            textAlign:'center'
+          }}>
+               <img src={item.image} style={{height:180}} />
+            <h4>{item.title}</h4>
+            <p>{item.description.slice(0,80)}</p>
+            <p style={{textTransform:'capitalize'}}>{item.category}</p>
+         
+          </div>
+        ))
+      }
      </div>
     </div>
   )
